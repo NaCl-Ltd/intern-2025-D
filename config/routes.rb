9bcd60525f9baf59356bc15d6bd333166c2455c5
fryoka <r.k.m.476376.mm@gmail.com>
2025-09-05 10:44:47 +0900

    細かな修正

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy] do
-     resource :likes, only: [:create, :destroy]
-     resource :favorites, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
     collection do
       get :latest
     end
@@ -29,8 +29,6 @@ Rails.application.routes.draw do
     end
   end
 
-
-  
   resources :relationships,       only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
 end
